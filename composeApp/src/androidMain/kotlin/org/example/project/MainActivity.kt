648cd8b4882449938835b01c491c8d09dbb72470
lucca <lbeurmann.dev@gmail.com>
2024-07-17 15:56:32 -0300

    feat: drawer ime padding sample

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -4,11 +4,13 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             App()
--- composeApp/src/commonMain/kotlin/calculator/CalculatorScreen.kt
+++ composeApp/src/commonMain/kotlin/calculator/CalculatorScreen.kt
@@ -1,38 +1,100 @@
 package calculator
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun CalculatorScreen() {
+    val state = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        modifier = Modifier.systemBarsPadding()
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Text(
-                text = buildAnnotatedString {
-                    withStyle(style = SpanStyle(color = Color.Red)) {
-                        append("Sample text")
+    BoxWithConstraints(Modifier.fillMaxSize()) {
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+                    Column {
+                        Text("Hello World")
                     }
-                },
-                color = Color.Blue
-            )
+                }
+            },
+            drawerState = state
+        ) {
+            Scaffold {
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .imePadding(),
+                    bottomBar = {
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                                .navigationBarsPadding()
+                        ) {
+                            Text("Hello World")
+                        }
+                    }
+                ) { paddings ->
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                top = paddings.calculateTopPadding() + 32.dp,
+                                bottom = paddings.calculateBottomPadding(),
+                                start = 20.dp,
+                                end = 20.dp
+                            )
+                    ) {
+                        item {
+                            Button(onClick = {
+                                scope.launch { state.open() }
+                            }) {
+                                Text("Open drawer")
+                            }
+                        }
+
+                        items((1..30).toList()) {
+                            Text(
+                                text = it.toString(),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            )
+                        }
+                        item {
+                            Spacer(Modifier.height(40.dp))
+                            OutlinedTextField(
+                                value = "",
+                                onValueChange = {}
+                            )
+                            Spacer(Modifier.height(40.dp))
+                        }
+                    }
+                }
+            }
         }
     }
 }
